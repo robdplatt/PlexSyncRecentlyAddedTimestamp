abf9a03de71e17583389aba71e493355950ffb9a
Rob Platt <rob@robplatt.com>
2023-04-07 13:33:46 -0700

    Add project files.

---
+++ PlexSyncRecentlyAddedTimestamp/Program.cs
@@ -0,0 +1,33 @@
+﻿using PlexSyncRecentlyAddedTimestamp.Repositories;
+using System.Data;
+
+//full path to your source database
+var SourceDB = "com.plexapp.plugins.library.db";
+//full path to your destination database
+var DestinationDB = "com.plexapp.plugins.library.db";
+
+var dt = SqliteRepository.ReadSQLiteData(SourceDB, "select media_items.created_at, metadata_items.guid  from media_items inner join media_parts on media_items.id = media_parts.media_item_id inner join metadata_items on metadata_items.id = media_items.metadata_item_id");
+var ids = SqliteRepository.ReadSQLiteData(DestinationDB, "select id,guid from metadata_items");
+
+int itemCounter = 0;
+foreach (DataRow row in dt.Rows)
+{
+    itemCounter++;
+    if (itemCounter % 100 == 0)
+        Console.WriteLine($"Processing item {itemCounter} / {dt.Rows.Count}");
+    string guid = row.Field<string>("guid")!;
+
+    if (!row.IsNull("created_at"))
+    {
+        Int64 createdAt = row.Field<Int64>("created_at");
+        var createdAtDT = DateTimeOffset.FromUnixTimeSeconds(createdAt);
+
+        var r = ids.Select($"guid='{guid.Replace("'", "''")}'").FirstOrDefault();
+        if (r != null)
+        {
+            Int64? destMediaId = r.Field<Int64>("id");
+            if (destMediaId.HasValue)
+                SqliteRepository.UpdateSQLiteData(DestinationDB, $"update media_items set created_at='{createdAtDT}' where id='{destMediaId}'");
+        }
+    }
+}
---
+++ PlexSyncRecentlyAddedTimestamp/Repositories/SqliteRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Sqlite;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SQLitePCL.raw;
+
+namespace PlexSyncRecentlyAddedTimestamp.Repositories;
+
+internal static class SqliteRepository
+{
+    internal static T? ReadSQLiteDataValue<T>(string dbPath, string sql)
+    {
+        SQLitePCL.Batteries.Init();
+
+        using (SqliteConnection conn = new($"Data Source={dbPath};Cache=Shared"))
+        {
+            conn.Open();
+            using SqliteCommand cmd = new(sql, conn);
+            var val = cmd.ExecuteScalar();
+            if (val != null)
+                return (T)val;
+        }
+
+        return default;
+    }
+
+    internal static DataTable ReadSQLiteData(string dbPath, string sql)
+    {
+        SQLitePCL.Batteries.Init();
+
+        DataTable dt = new();
+        using (SqliteConnection conn = new($"Data Source={dbPath};Cache=Shared"))
+        {
+            conn.Open();
+            using SqliteCommand cmd = new(sql, conn);
+            using SqliteDataReader reader = cmd.ExecuteReader();
+            dt.Load(reader);
+        }
+        return dt;
+    }
+
+    internal static void UpdateSQLiteData(string dbPath, string sql)
+    {
+        SQLitePCL.Batteries.Init();
+        using SqliteConnection conn = new($"Data Source={dbPath};Cache=Shared");
+        conn.Open();
+        using SqliteCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+    }
+}
